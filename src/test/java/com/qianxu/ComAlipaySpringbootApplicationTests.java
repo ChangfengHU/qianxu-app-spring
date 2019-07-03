e23ada625aab428d2e03247a56e542c9e2d53d76
ChangfengHU <faker_322@outlook.com>
2019-07-04 01:15:35 +0800

    前后端调通了

--- src/test/java/com/qianxu/ComAlipaySpringbootApplicationTests.java
+++ src/test/java/com/qianxu/ComAlipaySpringbootApplicationTests.java
@@ -1,10 +1,5 @@
 package com.qianxu;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,20 +15,20 @@ public class ComAlipaySpringbootApplicationTests {
 	}
 	@Test
 	public void testConnection() {
-//创建mongodb 客户端
-		MongoClient mongoClient = new MongoClient("localhost", 27017);
-//或者采用连接字符串
-MongoClientURI connectionString = new
-				MongoClientURI("mongodb://root:root@localhost:27017");
-//MongoClient mongoClient = new MongoClient(connectionString);
-//连接数据库
-		MongoDatabase database = mongoClient.getDatabase("test01");
-// 连接collection
-		MongoCollection<Document> collection = database.getCollection("student");
-//查询第一个文档
-		Document myDoc = collection.find().first();
-//得到文件内容 json串
-		String json = myDoc.toJson();
-		System.out.println(json);
+////创建mongodb 客户端
+//		MongoClient mongoClient = new MongoClient("localhost", 27017);
+////或者采用连接字符串
+//MongoClientURI connectionString = new
+//				MongoClientURI("mongodb://root:root@localhost:27017");
+////MongoClient mongoClient = new MongoClient(connectionString);
+////连接数据库
+//		MongoDatabase database = mongoClient.getDatabase("test01");
+//// 连接collection
+//		MongoCollection<Document> collection = database.getCollection("student");
+////查询第一个文档
+//		Document myDoc = collection.find().first();
+////得到文件内容 json串
+//		String json = myDoc.toJson();
+//		System.out.println(json);
 	}
 }
